ad94bb3559fdfd10e0b47df94a75058cee73fd48
unknown <rahulsg1508@gmail.com>
2021-02-23 13:17:33 +0530

    homepage layout

--- MEMORIES-POST-APP/client/src/App.js
+++ MEMORIES-POST-APP/client/src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {Container, AppBar, Typography, Grow, Grid} from "@material-ui/core";
+import Posts from "./components/Posts/Posts";
+import Form from "./components/Form/Form";
+import useStyles from "./styles";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    const classes = useStyles()
+
+    return (
+        <Container maxWidth = "lg">
+
+            <AppBar className={classes.appBar} position="static" color="inherit">
+
+                <Typography className={classes.heading} variant="h2" align="center">POST-MAN</Typography>
+
+                <img className={classes.image} src="https://i.ibb.co/DtQRbdF/writing.png" height="60" width="60" alt="icon"/>
+
+            </AppBar>
+
+            <Grow in>
+              <Container>
+                  <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+
+                      <Grid item xs={12} sm={7}>
+                        <Posts/>
+                      </Grid>
+
+                      <Grid item xs={12} sm={4}>
+                        <Form/>
+                      </Grid>
+
+                  </Grid>
+
+              </Container>
+            </Grow>
+
+        </Container>
+    );
 }
 
 export default App;
