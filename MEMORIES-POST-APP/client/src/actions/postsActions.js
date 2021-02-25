ce17f5918ab313a880f6c227b50ae5562c5ffc1f
unknown <rahulsg1508@gmail.com>
2021-02-25 11:51:32 +0530

    like post functionality

--- MEMORIES-POST-APP/client/src/actions/postsActions.js
+++ MEMORIES-POST-APP/client/src/actions/postsActions.js
@@ -59,3 +59,19 @@ export const deletePost = (id) => async (dispatch) => {
     console.log(error);
   }
 };
+
+// LIKE A POST
+export const likePost = (id) => async (dispatch) => {
+  try{
+
+    const { data } = await api.likePost(id);
+
+    dispatch({
+      type: "LIKE",
+      payload: data
+    });
+
+  } catch (error) {
+    console.log(error);
+  }
+};
--- MEMORIES-POST-APP/client/src/api/index.js
+++ MEMORIES-POST-APP/client/src/api/index.js
@@ -2,10 +2,17 @@ import axios from 'axios';
 
 const url = 'http://localhost:3001/posts';
 
+// FETCH ALL THE POSTS
 export const fetchPosts = () => axios.get(url);
 
+// CREATE A POST
 export const createPost = (newPost) => axios.post(url, newPost);
 
+// UPDATE A POST
 export const updatePost = (id, updatedPost) => axios.patch(`${url}/${id}`, updatedPost);
 
+// DELETE A POST
 export const deletePost = (id) => axios.delete(`${url}/${id}`);
+
+// LIKE A POST
+export const likePost = (id) => axios.patch(`${url}/${id}/likePost`);
